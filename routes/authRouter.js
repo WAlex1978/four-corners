a4f931f890e1f5a6cf56ded552797c03d8a91949
Alex Wong <fi0088@wayne.edu>
2019-04-16 18:16:09 -0700

    Login and jwt token

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,6 +1,7 @@
 const app = require('express').Router();
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const saltRounds = 10;
 
@@ -24,5 +25,25 @@ app.post('/register', async (req, res) => {
         res.send(err.toString());
     }
 });
+
+app.post('/login', async (req, res) => {
+    try {
+        const search = await User.find({username: req.body.username.toLowerCase()})
+        if (search.length === 0) {
+            throw new Error("Username not found");
+        }
+
+        const match = await bcrypt.compare(req.body.password, search[0].password);
+        if (!match) {
+            throw new Error("Incorrect password");
+        }
+
+        const token = await jwt.sign({username: req.body.username}, 'secretPW');
+        res.send(token);
+    }
+    catch (err) {
+        res.send(err.toString());
+    }
+})
 
 module.exports = app;
