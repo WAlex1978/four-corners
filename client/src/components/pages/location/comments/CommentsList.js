6daade2361529b94768e552f1b3d9a948217f125
Alex Wong <fi0088@wayne.edu>
2019-04-19 20:27:05 -0700

    Default avatar

--- client/src/components/pages/location/comments/CommentsList.js
+++ client/src/components/pages/location/comments/CommentsList.js
@@ -2,6 +2,8 @@ import React, { Fragment } from 'react';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
 import Divider from '@material-ui/core/Divider';
 import Typography from '@material-ui/core/Typography';
 import Fade from '@material-ui/core/Fade';
@@ -13,15 +15,17 @@ const CommentsList = (props) => {
                 <Fragment key={i}>
                     {i !== 0 ? <Divider/> : null}
                     <Fade in>
-                        <ListItem>
+                        <ListItem alignItems="flex-start">
+                            <ListItemAvatar>
+                                <Avatar src="/images/avatar.png"/>
+                            </ListItemAvatar>
                             <ListItemText 
                                 primary = { <Typography variant="subtitle1">{comment.name}</Typography> }
                                 secondary = {
                                     <Fragment>
                                         <Typography paragraph color="textPrimary">{comment.body}</Typography>
                                         <Typography variant="caption">{comment.date}</Typography>
-                                    </Fragment>
-                            } />
+                                    </Fragment>} />
                         </ListItem>
                     </Fade>
                 </Fragment>
